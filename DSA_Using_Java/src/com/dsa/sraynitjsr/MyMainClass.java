ce541e2d5b9a93f4393c49932b8cc3c6b126f1e4
Subhradeep Ray <subhradeepray2017@gmail.com>
2022-12-21 05:33:54 +0530

    initial commit

--- DSA_Using_Java/src/com/dsa/sraynitjsr/MyMainClass.java
+++ DSA_Using_Java/src/com/dsa/sraynitjsr/MyMainClass.java
@@ -2,8 +2,7 @@ package com.dsa.sraynitjsr;
 
 public class MyMainClass {
 	public static void main(String[] args) {
-		System.out.println("Starting Main...");
-		
+		System.out.println("Starting Main...");		
 		System.out.println("Ending Main.....");
 	}
 }
