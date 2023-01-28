e5403496025197deec37b71060b500e887164c30
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-01-28 23:22:50 +0530

    Stream Sorting based on Field

--- DSA_Using_Java/src/MyStreamAPIUsage.java
+++ DSA_Using_Java/src/MyStreamAPIUsage.java
@@ -9,21 +9,29 @@ class Product{
         this.name = name;  
         this.price = price;  
     }
-    public int compareTo(Product p) {
-        return id - p.id ;
+    public String getName() {
+        return this.name;
+    }
+    public float getPrice() {
+        return this.price;
+    }
+    public int getId() {
+        return this.id;
     }
 }  
 class MyStreamAPIUsage {  
     public static void start() {
         System.out.println("Inside Stream API Module");
         List<Product> products = new ArrayList<Product>();
-        products.add(new Product(1,"A",100f));
-        products.add(new Product(2,"B",200f));
-        products.add(new Product(3,"C",300f));
-        products.add(new Product(4,"D",400f));
-        products.add(new Product(5,"E",500f));
+        products.add(new Product(1,"Sachin",100f));
+        products.add(new Product(2,"Sourav",200f));
+        products.add(new Product(3,"Virat",300f));
+        products.add(new Product(4,"Dhone",400f));
+        products.add(new Product(5,"Rohit",500f));
         System.out.println(products.stream().filter(p -> p.price >300).map(p->p.price).collect(Collectors.toList()));
         System.out.println(products.stream().filter(p -> p.name == "B" || p.name == "C").map(p->p.id + " " +p.name + " " + p.price).collect(Collectors.toList()));
-        System.out.println(products.stream().sorted((p1, p2) -> p1.compareTo(p2)).collect(Collectors.toList()));
+        System.out.println(products.stream().sorted(Comparator.comparing(Product::getName).reversed()).map(p->p.id + " " +p.name + " " + p.price).collect(Collectors.toList()));
+        System.out.println(products.stream().sorted(Comparator.comparing(Product::getId)).map(p->p.id + " " +p.name + " " + p.price).collect(Collectors.toList()));
+        System.out.println(products.stream().sorted(Comparator.comparing(Product::getPrice).reversed()).map(p->p.id + " " +p.name + " " + p.price).collect(Collectors.toList()));  
     }
 }
