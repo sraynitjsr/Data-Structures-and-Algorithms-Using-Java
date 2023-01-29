712308ced5e9f48db3816890a889bccde5d5bea7
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-01-29 17:33:26 +0530

    Added Filter and Map Examples for Collection Objects i.e. Lists

--- DSA_Using_Java/src/MyStreamAPIUsage.java
+++ DSA_Using_Java/src/MyStreamAPIUsage.java
@@ -1,37 +1,54 @@
 import java.util.*;
-import java.util.stream.Collectors;  
-class Product{  
-    int id;  
-    String name;  
-    float price;  
-    public Product(int id, String name, float price) {  
-        this.id = id;  
-        this.name = name;  
-        this.price = price;  
+import java.util.stream.Collectors;
+
+class MyStreamAPIUsage {
+    
+    public static void printEvenNumbers() {
+        List<Integer> list = new ArrayList<>();
+        list.add(10); list.add(15); list.add(20); list.add(25); list.add(30);
+
+        System.out.println("\nPrinting Even Numbers From The List => " + list);
+        list.stream().filter(i -> i%2 == 0).collect(Collectors.toList()).forEach(evenNum -> System.out.print(evenNum + " "));
     }
-    public String getName() {
-        return this.name;
+
+    public static void filterExampleOne() {
+        List<String> list = new ArrayList<>();
+        list.add("Amit"); list.add("Ajay"); list.add("Sunil"); list.add("Kamal"); list.add("Ajit");
+
+        System.out.println("\n\nPrinting Names Which Starts With A => " + list);
+        list.stream().filter(name -> name.startsWith("A")).collect(Collectors.toList()).forEach(name -> System.out.print(name + " "));
+
+        System.out.println("\n\nPrinting Names Which Does Not Start With A => " + list);
+        list.stream().filter(name -> !name.startsWith("A")).collect(Collectors.toList()).forEach(name -> System.out.print(name + " "));
     }
-    public float getPrice() {
-        return this.price;
+
+    public static void mapExampleOne() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1); list.add(2); list.add(3); list.add(4); list.add(5);
+
+        System.out.println("\n\nPrinting Square of The List Elements => " + list);
+        list.stream().map(data -> data * data).collect(Collectors.toList()).forEach(data -> System.out.print(data + " "));
     }
-    public int getId() {
-        return this.id;
+    
+    public static void exampleFilterAndMapData() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1); list.add(5); list.add(6); list.add(9); list.add(8);
+        
+        System.out.println("\n\nFilter Data and Square Them, Filter Only Odd Numbers Here => " + list);
+        list.stream().filter(data -> data%2 != 0).map(data -> data * data).collect(Collectors.toList()).forEach(data -> System.out.print(data + " "));
     }
-}  
-class MyStreamAPIUsage {  
+
     public static void start() {
-        System.out.println("Inside Stream API Module");
-        List<Product> products = new ArrayList<Product>();
-        products.add(new Product(1,"Sachin",100f));
-        products.add(new Product(2,"Sourav",200f));
-        products.add(new Product(3,"Virat",300f));
-        products.add(new Product(4,"Dhone",400f));
-        products.add(new Product(5,"Rohit",500f));
-        System.out.println(products.stream().filter(p -> p.price >300).map(p->p.price).collect(Collectors.toList()));
-        System.out.println(products.stream().filter(p -> p.name == "B" || p.name == "C").map(p->p.id + " " +p.name + " " + p.price).collect(Collectors.toList()));
-        System.out.println(products.stream().sorted(Comparator.comparing(Product::getName).reversed()).map(p->p.id + " " +p.name + " " + p.price).collect(Collectors.toList()));
-        System.out.println(products.stream().sorted(Comparator.comparing(Product::getId)).map(p->p.id + " " +p.name + " " + p.price).collect(Collectors.toList()));
-        System.out.println(products.stream().sorted(Comparator.comparing(Product::getPrice).reversed()).map(p->p.id + " " +p.name + " " + p.price).collect(Collectors.toList()));  
+        System.out.println("\nInside Stream API Module");
+        
+        printEvenNumbers();
+        
+        filterExampleOne();
+
+        mapExampleOne();
+
+        exampleFilterAndMapData();
+
+        System.out.println("");
     }
 }
